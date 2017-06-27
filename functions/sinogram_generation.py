63873bc3ca7ac625464512b8debd0252633cda81
Bo Gao <starbury@n175-p19.kthopen.kth.se>
2017-06-27 10:26:41 +0200

    Modify the code to fix one bug with Linux System

--- functions/sinogram_generation.py
+++ functions/sinogram_generation.py
@@ -53,7 +53,7 @@ def sino_gene(DICOM_path, **kwargs):
     # image truncating
     flag = 0
     
-    # Read in necessary  the user would like to perform projection image truncating
+    # Read in if the user would like to perform projection image truncating
     roi_min = kwargs.pop('roi_min', None)
     if roi_min is not None:
         flag += 1
@@ -63,7 +63,8 @@ def sino_gene(DICOM_path, **kwargs):
     if roi_max is not None:
         flag += 1
         a1 = np.array(roi_max)
-
+    
+    # These two variables need to be given in order to perform projection image processing
     LightFieldPath = kwargs.pop('LightFieldPath', None)
     if LightFieldPath is not None:
         LF_ds = dicom.read_file(LightFieldPath)
@@ -71,14 +72,14 @@ def sino_gene(DICOM_path, **kwargs):
     Log = kwargs.pop('Log', None)
     
     # Check if the reconstruction space is centered in the origin point
-    if roi_max[0] + roi_min[0] != 0 or roi_max[1] + roi_min[1] == 0 \
-    or roi_max[0] + roi_min[0] == 0:
+    if roi_max[0] + roi_min[0] != 0 or roi_max[1] + roi_min[1] != 0 \
+    or roi_max[0] + roi_min[0] != 0:
         raise ValueError('Expect the reconstruction space to be centered at (0,0,0), \
                          got{}'.format((a+a1)/2))
     
     # No checking procedure for these three variables, as I believe no one is dumb
     # enough to input a matrix when they are supposed to input a constant
-    if (flag != 0 or flag != 2):
+    if (flag != 0 and flag != 2):
         raise ValueError('Detect the attempt to perform projection image truncating, \
                          while the input paramter is not enough, got {}'.format(flag) + \
                          ' while expect 2')
@@ -104,8 +105,7 @@ def sino_gene(DICOM_path, **kwargs):
     l2 = ds.ReconstructionPixelSpacing[1]
     
     ProjectionGeometry = ds.TypeofProjectionGeometry
-    number =  len([name for name in os.listdir(DICOM_path) if os.path.isfile(\
-                           os.path.join(DICOM_path, name))])
+    number = ds.NumberOfFramesInRotation
     
     if (flag == 2):
         # When flag equals 2, truncating needs to be done in projection image, 
@@ -128,20 +128,26 @@ def sino_gene(DICOM_path, **kwargs):
             
             L2 = (src+det)/(src-np.sqrt(roi_max[0]**2+roi_max[1]**2))*roi_max[2]*1/l2 + 5
             # Ensure the truncated region will not be larger than the collected projection image
-            L1 = np.clip(L1,1,ds.Columns/2)
-            L2 = np.clip(L2,1,ds.Rows/2)
+            L1 = np.int(np.clip(L1,1,ds.Columns/2))
+            L2 = np.int(np.clip(L2,1,ds.Rows/2))
             
             sinogram = np.zeros([number, 2*L1, 2*L2])
-            i = 0
-            for filename in glob.glob(os.path.join(DICOM_path, '*.dcm')):
+            # I would like to use this, but this command cannot guarantee to read in files in the right order
+            #for filename in glob.glob(os.path.join(DICOM_path, '*.dcm')):
+            for i in range(number):
+                filename = DICOM_path + '/projection_image' + str(i+1) + '.dcm'
                 ds = dicom.read_file(filename)
                 img = ds.pixel_array
-                img = -np.log(img/LightField)
-                img[img<0] = 0
-                img[np.isnan(img)] = 0
-                img[np.isinf(img)] = 0
+                if LightFieldPath is not None:
+                    img = img/LightField
+                    img[np.isnan(img)] = 1
+                    img[np.isinf(img)] = 1
+                if Log is not None:
+                    img = -np.log(img)
+                    img[img<0] = 0
+                
                 sinogram[i,:,:] = img[center_1-L1:center_1+L1, center_2-L2:center_2+L2]
-                i += 1
+                print(str(i) + ' projection images have been included')
     
         if ProjectionGeometry == 'FANBEAM':
             # Calculate the range of reconstruction space at extreme cases
@@ -155,57 +161,71 @@ def sino_gene(DICOM_path, **kwargs):
             L1_45 = (src+det)/(src) * np.sqrt(2) * Len * 1/l1 + 5
             
             L1 = max(L1_45, L1_90_1, L1_90_2)
-            L1 = np.clip(L1,1,ds.Columns/2)
+            L1 = np.int(np.clip(L1,1,ds.Columns/2))
             # L1 = ((src+det)/src) * np.sqrt(roi_max[0]**2 + roi_max[1]**2)/l1
             sinogram = np.zeros([number, 2*L1])
-            i = 0
-            for filename in glob.glob(os.path.join(DICOM_path, '*.dcm')):
+            #for filename in glob.glob(os.path.join(DICOM_path, '*.dcm')):
+            for i in range(number):
+                filename = DICOM_path + '/projection_image' + str(i+1) + '.dcm'
                 ds = dicom.read_file(filename)
                 img = ds.pixel_array
-                img = -np.log(img/LightField)
-                img[img<0] = 0
-                img[np.isnan(img)] = 0
-                img[np.isinf(img)] = 0
+                if LightFieldPath is not None:
+                    img = img/LightField
+                    img[np.isnan(img)] = 1
+                    img[np.isinf(img)] = 1
+                if Log is not None:
+                    img = -np.log(img)
+                    img[img<0] = 0
                 sinogram[i,:] = img[center_1-L1:center_1+L1]
-                i += 1
         
         if ProjectionGeometry == 'PARALLEL2D':
             L1 = np.sqrt(roi_max[0]**2 + roi_max[1]**2)/l1
-            L1 = np.clip(L1,1,ds.Columns/2)
+            L1 = np.int(np.clip(L1,1,ds.Columns/2))
             sinogram = np.zeros([number, 2*L1])
             i = 0
-            for filename in glob.glob(os.path.join(DICOM_path, '*.dcm')):
+            #for filename in glob.glob(os.path.join(DICOM_path, '*.dcm')):
+            for i in range(number):
+                filename = DICOM_path + '/projection_image' + str(i+1) + '.dcm'
                 ds = dicom.read_file(filename)
                 img = ds.pixel_array
-                img = -np.log(img/LightField)
-                img[img<0] = 0
-                img[np.isnan(img)] = 0
-                img[np.isinf(img)] = 0
+                if LightFieldPath is not None:
+                    img = img/LightField
+                    img[np.isnan(img)] = 1
+                    img[np.isinf(img)] = 1
+                if Log is not None:
+                    img = -np.log(img)
+                    img[img<0] = 0
                 sinogram[i,:] = img[center_1-L1:center_1+L1]
-                i += 1
             
         if ProjectionGeometry == 'PARALLEL3D':
             L1 = np.sqrt(roi_max[0]**2 + roi_max[1]**2)/l1
             L2 = roi_max[2]/l2
-            L1 = np.clip(L1,1,ds.Columns/2)
-            L2 = np.clip(L2,1,ds.Rows/2)
+            L1 = np.int(np.clip(L1,1,ds.Columns/2))
+            L2 = np.int(np.clip(L2,1,ds.Rows/2))
             
             sinogram = np.zeros([number, 2*L1, 2*L2])
             i = 0
-            for filename in glob.glob(os.path.join(DICOM_path, '*.dcm')):
+            #for filename in glob.glob(os.path.join(DICOM_path, '*.dcm')):
+            for i in range(number):
+                filename = DICOM_path + '/projection_image' + str(i+1) + '.dcm'
                 ds = dicom.read_file(filename)
                 img = ds.pixel_array
-                img = -np.log(img/LightField)
-                img[img<0] = 0
-                img[np.isnan(img)] = 0
-                img[np.isinf(img)] = 0
+                if LightFieldPath is not None:
+                    img = img/LightField
+                    img[np.isnan(img)] = 1
+                    img[np.isinf(img)] = 1
+                if Log is not None:
+                    img = -np.log(img)
+                    img[img<0] = 0
                 sinogram[i,:,:] = img[center_1-L1:center_1+L1, center_2-L2:center_2+L2]
-                i += 1
+
     else:
         # No truncating is performed in this case
         sinogram = np.zeros([number, 2*L1, 2*L2])
-        i = 0
-        for filename in glob.glob(os.path.join(DICOM_path, '*.dcm')):
+        
+        #for filename in glob.glob(os.path.join(DICOM_path, '*.dcm')):
+        for i in range(number):
+            filename = DICOM_path + '/projection_image' + str(i+1) + '.dcm'
             ds = dicom.read_file(filename)
             img = ds.pixel_array
             img = -np.log(img/LightField)
@@ -213,7 +233,6 @@ def sino_gene(DICOM_path, **kwargs):
             img[np.isnan(img)] = 0
             img[np.isinf(img)] = 0
             sinogram[i,:,:] = img
-            i += 1
             
     return sinogram, ds
     
